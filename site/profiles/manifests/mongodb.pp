6dea9892ca1e904992f7c4d4e70e9ce251e24eb5
root <root@sky-puppet-master.webterrorist.net>
2016-07-09 13:43:35 +0000

    mongodb hiera lookups based per host

--- site/profiles/manifests/mongodb.pp
+++ site/profiles/manifests/mongodb.pp
@@ -1,3 +1,6 @@
+#
+# this is an example what is based on the shipped example. But we are using hiera instead. Because to show you what a can do
+#
 class profiles::mongodb {
   class { '::mongodb::globals':
     manage_package_repo => true
@@ -7,4 +10,9 @@ class profiles::mongodb {
     bind_ip    => ['0.0.0.0'],
     replset    => 'rsmain'
   }
+  mongodb_replset{'rsmain':
+    members => ['mongo1:27017', 'mongo2:27017', 'mongo3:27017' ],
+    arbiter => 'mongo3:27017',
+  }
+}
 }
